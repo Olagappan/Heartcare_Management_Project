e5bae08687e8141ebdae97cba60cc4581cc236b3
Ganesh R <olagappan.22@gmail.com>
2023-01-31 15:57:33 +0530

    changes made

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -79,12 +79,9 @@ WSGI_APPLICATION = 'heartcare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Heartcare_Management_DB',
-        'USER': 'root',
-        'PASSWORD': 'admin',
-        'HOST':'localhost',
-        'PORT':3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+
     }
 }
 
